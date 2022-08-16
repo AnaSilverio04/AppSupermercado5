3e59f9f39e8fb0be57d4c4b54f07ba6b30832fe8
Usuario <Usuario@CPD02-M09>
2022-08-16 12:19:29 -0300

    Adicionar arquivos de projeto.

---
+++ AppSupermercado5/AppSupermercado5/MainPage.xaml.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace AppSupermercado5
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage(Entry valor)
+        {
+            InitializeComponent();
+            this.Valor = valor;
+        }
+    }
+}
